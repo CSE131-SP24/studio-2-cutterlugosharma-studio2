f06d0164f5d42fd285460ec1170860fd01e242a2
ash3 <ash3@localhost>
2024-02-01 11:52:54 -0600

    did it 2

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -3,8 +3,13 @@ package studio2;
 public class Ruin {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Double startAmount;
+		Double winChance;
+		Double winLimit;
+		
+		System.out.println("How much money are you starting with?");
+		//Double startAmount = in.nextLine(); 
+		
 	}
 
 }
