5af24b8ae044365ea778a6ea415a51c3c90a8c21
ash3 <ash3@localhost>
2024-02-01 12:46:40 -0600

    Final Push

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -1,15 +1,47 @@
 package studio2;
 
+import java.util.Scanner;
+
 public class Ruin {
 
 	public static void main(String[] args) {
-		Double startAmount;
-		Double winChance;
-		Double winLimit;
 		
-		System.out.println("How much money are you starting with?");
-		//Double startAmount = in.nextLine(); 
+		Scanner in = new Scanner(System.in);
 		
+		int startAmount = 2; 
+		double winChance = 0.7;
+		int winLimit = 10;
+		int totalSimulations = 500;
+		
+		for (int x = totalSimulations; x > 0; x--)
+		{
+			while (startAmount < winLimit && startAmount != 0)
+				
+			{
+				double randomNumber = Math.random();
+				
+				if(randomNumber <= winChance)
+				{
+					System.out.println("Win");
+					startAmount += 1;
+				}
+				else 
+				{
+					System.out.println("Lose");
+					startAmount -= 1;
+				}
+			}
+		}
+		
+		
+		if (startAmount >= winLimit)
+		{
+			System.out.println("Success");
+		}
+		 if (startAmount == 0)
+		 {
+			System.out.println("Ruin");
+		 }
 	}
 
 }
